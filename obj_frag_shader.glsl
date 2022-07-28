ef69237f628674b584458c4dcd90af5ec0d763b4
ootisg <ootisg@illcomics.com>
2022-07-27 22:30:37 -0500

    Ambient

--- obj_frag_shader.glsl
+++ obj_frag_shader.glsl
@@ -9,6 +9,9 @@ uniform sampler2D tex2;
 
 uniform vec3 lightPos;
 
+vec3 objColor = vec3 (1.0, 0.5, 0.31);
+float ambient = 0.1;
+
 void main() {
-	FragColor = vec4 (1.0, 0.5, 0.31, 1.0);
+	FragColor = vec4 (ambient * objColor, 0.0);
 }
